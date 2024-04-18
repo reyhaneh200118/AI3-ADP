2042f7d55655b6547b307c2ed4dd54184ac1c4bf
Kian Panahi <kian.panahi@haw-hamburg.de>
2024-04-18 10:40:59 +0200

    gelöst: TeilA

--- Java/src/aufgabe1/TeilA.java
+++ Java/src/aufgabe1/TeilA.java
@@ -1,42 +1,54 @@
 package aufgabe1;
 
 
+import java.util.Arrays;
+
 public class TeilA {
 
     public static int[] localMax(int[] ary,int radius) {
-        return findLocalMax(ary,0,ary.length-1,radius);
+        return findLocalMax(ary,0,ary.length,radius);
     }
 
-    private static int[] findLocalMax(int[] ary, int lo, int hi, int radius) {
-        if (lo > hi) return null;
-
-        int mid = lo + (hi + lo) / 2;
+    private static int[]findLocalMax(int[] ary, int lo, int hi, int radius){
+        if (lo >= hi || lo < 0 || hi > ary.length || radius < 0) return null;
+        int mid = lo + (hi - lo) / 2;
         boolean isLocalMax = true;
-        for (int j = mid-radius; j < mid+radius; j++) {
-            if (j < mid && ary[j] >= ary[j+1]) {
-                isLocalMax = false;
-                break;
-            }
-            if (j > mid && ary[j] <= ary[j+1]) {
+
+        for (int i = 0; i < radius; i++) {
+            if (mid-i-1 < 0 || mid+i+1 >= ary.length) return null;
+            if (ary[mid-i] <= ary[mid-i-1] || ary[mid+i] <= ary[mid+i+1]) {
                 isLocalMax = false;
                 break;
             }
         }
+
         if (isLocalMax) {
-            return new int[] {Math.max(lo, mid - radius), Math.min(hi, mid + radius)};
-        } else if (ary[mid - 1] > ary[mid]) {
-            return findLocalMax(ary, radius, lo, mid - 1); // Linke Hälfte
+            return Arrays.copyOfRange(ary,mid-radius,mid+radius+1);
         } else {
-            return findLocalMax(ary, radius, mid + 1, hi); // Rechte Hälfte
+            lo = ary[mid-1] <= ary[mid+1] ? lo : mid;
+            hi = lo + mid;
+            return findLocalMax(ary, lo, hi, radius);
         }
+
     }
+
     public static void main(String[] args) {
         int[] ary1 = {1, 61, 89, 75, 16, 33, 89, 59, 28, 3, 3, 97, 61, 85, 47, 38, 78, 7, 6, 15};
-        int[] result = localMax(ary1, 2);
-        if (result != null) {
-            System.out.println("Berechnet der Aufruf von localMax(ary,2): [" + result[0] + ", " + result[1] + "]");
-        } else {
-            System.out.println("Kein lokales Maximum gefunden.");
-        }
+        int[] ary2 = {1, 61, 16, 75, 89, 133, 89, 59, 28, 3, 3, 97, 61, 85, 47, 38, 78, 7, 6, 15};
+        int[] ary3 = {99,1, 61, 89, 75, 16, 33, 89, 59, 28, 3, 3, 97, 61, 85, 47, 38, 78, 7, 6, 15};
+        int[] result;
+
+        //ary1
+        result = localMax(ary1, 2);
+        System.out.println(Arrays.toString(result));
+
+        //ary2
+        result = localMax(ary2, 3);
+        System.out.println(Arrays.toString(result));
+
+        //ary3
+        result = localMax(ary3, 1);
+        System.out.println(Arrays.toString(result));
+
     }
 }

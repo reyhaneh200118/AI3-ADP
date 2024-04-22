beee5c5c7faa6ef6681ff4bad0fa6af80e7868ac
Kian Panahi <kian.panahi@haw-hamburg.de>
2024-04-22 11:17:54 +0200

    NumberGenerator angepasst mit Princeton libraries

--- Java/src/aufgabe1/NumberGenerator.java
+++ Java/src/aufgabe1/NumberGenerator.java
@@ -1,33 +1,28 @@
 package aufgabe1;
 
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Random;
 
 public class NumberGenerator {
-    private Random rand;
-
-    public NumberGenerator(Random rand) {
-        this.rand = rand;
-    }
-
-    public void generateNumber(int n, int min, int max) {
-        if (min >= max || n < 0 || ((long) max - min >= Integer.MAX_VALUE)) throw new IllegalArgumentException();
-
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                int intNum = rand.nextInt((max - min) + 1) + min;
-                StdOut.println(intNum);
-            } else {
-                double doubleNum = rand.nextDouble() * (max - min) + min;
-                StdOut.println(doubleNum);
-            }
-        }
-    }
 
     public static void main(String[] args) {
-        NumberGenerator numberGenerator = new NumberGenerator(new Random());
-        numberGenerator.generateNumber(10,0,100);
+        //args: 10 0 10
+        int N = Integer.parseInt(args[0]);
+        double min = Double.parseDouble(args[1]);
+        double max = Double.parseDouble(args[2]);
+
+        for (int i = 0; i < N; i++) {
+            if (i % 2 == 0) {
+                int x = StdRandom.uniformInt((int) min, (int) max); //Achtung: Intervall ist [min, max), nicht [min, max] wie gefordert
+                StdOut.print(x + " ");
+            } else {
+                double x = StdRandom.uniformDouble(min, max);
+                StdOut.printf("%.2f ",x);
+            }
+        }
+        StdOut.println();
     }
 
 }
